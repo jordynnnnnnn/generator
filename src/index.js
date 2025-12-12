ba82dbb35f7ee25040747a4dfbb9c2e7a8335d3f
jordynnnnnnn <jordyn.pilcher@hotmail.com>
2025-12-13 00:20:39 +1030

    Added AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "Example: Chicken salad 🐓",
-    autoStart: true,
-    delay: "natural",
-  });
+  let instructionInput = document.querySelector("#ingredient-input");
+  let apiKey = "ca3401aa9fo6993fde3bf6t53aa16d30";
+  let prompt = `Generate a simple recipe from ${instructionInput.value} as the main ingredient. Include ingredients and instructions.`;
+  let context =
+    "You are a world class chef that creates easy to follow recipes. You provide clear instructions and a concise list of ingredients. All ingredients are in list format with <br /> tags between each item. The instructions are in numbered format with <br /> tags between each step. Please have ingredients headline and instructions headline within <h4> tags. Please add a title to the recipe at the top within <h2> tags.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeForm = document.querySelector("#recipe-generator-form");
